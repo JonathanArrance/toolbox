2149e30892df4d86a4714a6d753846517e5f8e0c
Jonathan Arrance <JonathanArrance@users.noreply.github.com>
2018-08-18 21:55:22 -0400

    Update centos-setup.sh

--- Docker/centos-setup.sh
+++ Docker/centos-setup.sh
@@ -1,1 +1,11 @@
-
+#!/bin/bash
+sudo yum install -y epel-release
+sudo yum install -y git
+sudo yum install -y easy_install
+sudo easy_install pip
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install -y docker-ce
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo service docker start
